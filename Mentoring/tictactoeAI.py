354508dd1bf0574de09e7cfd96a1b99854a33545
Penelope Nguyen <thuytien.nguyen01@utrgv.edu>
2016-10-13 01:37:44 -0500

    AI works to win/tie. removed functions
    AI started cheating and playing illegal moves. fixed that. actually, i completely reworked the AI so that it should either win or tie with the player. code needs to be cleaned up a little still. 
    
    removed all functions so that students will better understand code.

--- Mentoring/tictactoeAI.py
+++ Mentoring/tictactoeAI.py
@@ -8,52 +8,13 @@ winStates = [ [0,1,2],
               [2,5,8],
               [0,4,8],
               [2,4,6]
-             ] 
+             ]
 
+diamond = [1, 3, 5, 7]
 corners = [0, 2, 6, 8]
-center = 4 
+center = 4
+availableSpaces = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-def printBoard(aBoard):
-    print ()
-    for row in board:
-        print (" | ".join(row))
-        
-def compAI(userSpaces, compSpaces):
-
-    allSpaces = userSpaces + compSpaces
-    
-    if center not in allSpaces: 
-        move = center 
-    else:
-        for state in winStates:
-            compWinPoss = 0 
-            for num in state:
-                if num in compSpaces:
-                    winPoss += 1
-            if winPoss > 1:
-                return state not in compSpaces:
-            
-    return move 
-    
-            
-def checkWin(userSpace, compSpace):
-    winner = "__NO ONE__" 
-    winStates = [ [0,1,2],
-                  [3,4,5],
-                  [6,7,8],
-                  [0,3,6],
-                  [1,4,7],
-                  [2,5,8],
-                  [0,4,8],
-                  [2,4,6]
-                 ]
-    for state in winStates:
-        if (compSpace == state) or (set(state) < set(compSpace)):
-           winner = "__THE COMPUTER__" 
-        if (userSpace == state) or (set(state) < set(userSpace)):
-            winner = "__YOU__"
-    return winner
-    
 board = [ ['0', '1', '2'] , ['3', '4', '5'] , ['6', '7', '8'] ] 
     
 playersMoves = [ ] 
@@ -61,6 +22,8 @@ user = ''
 comp = ''
 userSpaces = []
 compSpaces = []
+winner = "__NO ONE__" 
+
 user = input('would you like to be x or o? ')
 while (user != 'x') and  (user != 'o'): 
     user = input('that is not a valid choice. please try again: ')
@@ -69,25 +32,36 @@ if user == 'x':
     comp = 'o'
 else:
     comp = 'x' 
+
 userTurn = input("would you like to be player one or player two? please enter one or two: ")
 while (userTurn  != "one") and (userTurn != "two"): 
-    userTurn = input("that is not a valid choice. the accepted answers are one or one: ")
+    userTurn = input("that is not a valid choice. the accepted answers are one or two: ")
 if userTurn == "one":
     playersMoves.append(['user', user])
     playersMoves.append(['comp', comp]) 
 else:
     playersMoves.append(['comp', comp]) 
-    playersMoves.append(['user', user]) 
-print("these are the places where you can enter moves:")
-printBoard(board)
-for turn in range(9):
-    if turn >= 4:  
-        winner = checkWin(compSpaces, userSpaces)
-        if winner != "__NO ONE__":
-            break
+    playersMoves.append(['user', user])
+
     
-    currentPlayer = playersMoves[turn%2][0] 
+print("these are the places where you can enter moves:\n")
+
+for row in board: 
+    print (" | ".join(row))
+    
+for turn in range(9):
+
+    if winner != "__NO ONE__":
+        break
+    
+    currentPlayer = playersMoves[turn%2][0]
+
     if currentPlayer == 'user':
+        if (turn >= 4):
+            for state in winStates:
+                if (set(userSpaces) == set(state)) or (set(state) < set(userSpaces)):
+                    winner = "__YOU__"
+                
         move = playersMoves[turn%2][1] 
         print ("\n//IT'S THE USER'S TURN//")
         while True:
@@ -102,19 +76,87 @@ for turn in range(9):
                 break 
             except ValueError:
                 print ("that is not a valid number. Please try again. ")
-        userSpaces.append(space) 
-        
+
+        userSpaces.append(space)
     
     else:
         print("\n//IT'S THE COMPUTER'S TURN//")
-        move = playersMoves[turn%2][1] 
-        space = compAI(turn, userSpaces, compSpaces)
-        compSpaces.append(space) 
+        move = playersMoves[turn%2][1]
         
+        space = -1
+        winSpace = -2 
+        defendSpace =  -3
+        
+        if len(compSpaces) == 0:
+            space = random.choice(corners)
+            
+        elif len(userSpaces) == 1:
+            if (userSpaces[0] in corners):
+                if (compSpaces[0] in corners):
+                    space = center
+                    center = -4
+                else:
+                    while (space not in availableSpaces):
+                        space = random.choice(corners) 
+            else:
+                while (space not in availableSpaces): 
+                    space = random.choice(corners)
+        else:
+            
+            for state in winStates:
+                compAvailSpaces = state.copy()
+                userAvailSpaces = state.copy() 
+            
+                winLevel = 0
+                threatLevel = 0 
+
+                for num in state:
+                    if (num in compSpaces):
+                        winLevel += 1
+                        compAvailSpaces.remove(num)
+                        
+                    if (num in userSpaces):
+                        threatLevel +=1
+                        userAvailSpaces.remove(num)
+
+                if (winLevel >= 2):
+                    if (compAvailSpaces[0] in availableSpaces):
+                        winSpace = compAvailSpaces[0]
+                        
+                elif (threatLevel >=2):
+                    if (userAvailSpaces[0] in availableSpaces):
+                        defendSpace = userAvailSpaces[0]
+
+            if winSpace >= 0:
+                space = winSpace
+                winner = "__THE COMPUTER__" 
+            
+            elif defendSpace >=0:
+                space = defendSpace
+
+            else:
+                while (space not in availableSpaces): 
+                    if len(corners) != 0:
+                        space = random.choice(corners)
+                        corners.remove(space)
+                    elif center >= 0:
+                        space = center
+                        center = -4
+                    else:
+                        space = random.choice(diamond)
+                        diamond.remove(space) 
+                     
+        compSpaces.append(space)
+
+        
+    availableSpaces.remove(space) 
+
     row = space//3
     column = space%3 
     board[row][column] = move
     
-    printBoard(board)
+    print ()
+    for row in board: 
+        print (" | ".join(row))
     
 print ("THE WINNER IS: ", winner) 
