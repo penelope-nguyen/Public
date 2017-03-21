dc8086708e80b17d19940bc7c57850edd27dd97f
Penelope Nguyen <thuytien.nguyen01@utrgv.edu>
2017-03-21 15:27:58 -0500

    it works now

--- CSCI 3333 - CompSci 3 - C++/mergeSort.h
+++ CSCI 3333 - CompSci 3 - C++/mergeSort.h
@@ -28,7 +28,7 @@ private:
 	void split(node * head1, node * tail2, node * & tail1, node * & head2, int num_nodes) {
 
 		// t2 being the original tail 
-	
+
 		node * traverse = head1;
 
 		int mid = num_nodes / 2 - 1;
@@ -40,62 +40,74 @@ private:
 		tail1 = traverse;
 		head2 = traverse->next;
 
-
 		head2->prev = NULL;
 		tail1->next = NULL;
 
-	}
-	
-	void merge(node * & head1, node * tail1, node * & head2, node * & tail2) {
 		
-		if (head1 == head2)
-			return;
+	}
 
-		linkedList<unsigned> merged;
+	void merge(node * & head1, node * tail1, node * head2, node * & tail2) {
+
+		node * trv = head1;
+
+		trv = head2;
 		node * trv_1 = head1;
 		node * trv_2 = head2;
-		node * new_trv;
-		while (trv_1 != NULL && trv_2 != NULL) { 
-			
-			if (trv_1->data < trv_2->data) {
 
-				merged.push_back(trv_1->data);
+		node * new_head;
+		node * new_tail;
+
+		if (trv_1->data < trv_2->data) {
+			new_head = trv_1;
+			trv_1 = trv_1->next;
+		}
+		else {
+			new_head = trv_2;
+			trv_2 = trv_2->next;
+		}
+		new_tail = new_head;
+
+		
+		while (trv_1 != NULL && trv_2 != NULL) {
+	
+			if (trv_1->data < trv_2->data) {
+				new_tail->next = trv_1;
 				trv_1 = trv_1->next;
 			}
-			else { 
-				merged.push_back(trv_2->data);
+			else {	
 
+				new_tail->next = trv_2;
 				trv_2 = trv_2->next;
 			}
-			
-			new_trv = new_trv->next;
+
+			new_tail = new_tail->next;
 		}
 
 		if (trv_1 == NULL) {
-
-			while (trv_2 != NULL) {
-				merged.push_back(trv_2->data);
-				trv_2 = trv_2->next;
-			}
+			new_tail->next = trv_2;
 		}
 
 		if (trv_2 == NULL) {
-			while (trv_1 != NULL) {
-				merged.push_back(trv_1->data);
-				trv_1 = trv_1->next;
-			}
+			new_tail->next = trv_1;
 		}
 
-		head1 = merged.head;
-		tail2 = merged.tail;
+		while (new_tail->next != NULL) {
+			new_tail = new_tail-> next;
+		}
+		head1 = new_head;
+		tail2 = new_tail;
+
+		trv = new_head;
 	}
 
 	//merge sort the list from node h to t
 	void mergeSort(node * &h, node * &t, int num_nodes)
 	{
 
+
 		if (h != NULL && h != t) //2 or more items, otherwise base case of nothing
 		{
+
 			//step 1: split list into 2
 			//point t1 and h2 to nodes such that: 
 			//the left half of the list is from node h to t1, right half from node h2 to t
@@ -104,13 +116,17 @@ private:
 			split(h, t, t1, h2, num_nodes);
 
 			//step 2: sort 2 halves
-			mergeSort(h, t1, num_nodes/2);
-		
-			mergeSort(h2, t, num_nodes/2);
+			
+			int split_num_nodes = num_nodes / 2;
+			mergeSort(h, t1, split_num_nodes);
+			
+			mergeSort(h2, t, split_num_nodes);
 
 			//step 3: merge sorted halves back together
 			//into a single sorted list
+
 			merge(h, t1, h2, t);
+		
 		}
 	}
 
@@ -132,7 +148,7 @@ public:
 			pop_front();
 		}
 	}
-	
+
 
 	int size()
 	{
@@ -197,5 +213,5 @@ public:
 	{
 		mergeSort(head, tail, numItems);
 	}
-	
+
 };
